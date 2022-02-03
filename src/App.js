5da98dba12bc7fa4762cf99b9410b37e7bf0f59b
Tenzin <tenthinten@gmail.com>
2022-02-03 21:28:35 +0530

    create path with different pages with router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.scss';
 import React from "react";
-import {BrowerRouter as Router, Route, Switch} from "react-router-dom";
-
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./components/Home/Home"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import PageNotFound from "./components/PageNotFound/PageNotFound"
+import MovieDetail from "./components/MovieDetail/MovieDetail"
 
 function App() {
   return (
-    <div className="App">
-        Hello
+    <div className="app">
+        <Router>
+          <Header></Header>
+          <Route path="/" component={Home}/>
+          <Route path="/movie/:imdbID" component={MovieDetail}/>
+          <Route component={PageNotFound}/>
+          <Footer/>
+        </Router>
     </div>
   );
 }
