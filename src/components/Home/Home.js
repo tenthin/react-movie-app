1fba9c246c3ab41852d007bb7b1d6fdc66d2efb6
Tenzin <tenthinten@gmail.com>
2022-02-04 20:36:28 +0530

    api call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from "../MovieListing/MovieListing"
+import movieApi from '../../common/apis/movieApi';
+import {APIKey} from "../../common/apis/MovieApiKey"
 
 const Home = () => {
+
+    useEffect(() => {
+        const movieText = "Harry";
+        const fetchMovies = async () => {
+            const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+            .catch((err) => {
+                console.log("Err :", err);
+            });
+            console.log("response from API",response)
+        }
+        fetchMovies();
+    },[])
     return (
         <div>
-            Home
+        <div className='banner-img'></div>
+            <MovieListing/>
         </div>
     );
 };
