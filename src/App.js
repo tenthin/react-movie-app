730949e1977eade2b7c3321785fa17c241af61b8
Tenzin <tenthinten@gmail.com>
2022-02-04 18:41:04 +0530

    add footer scss

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ function App() {
     <div className="app">
         <Router>
           <Header></Header>
+          <div className='container'>
           <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/movie/:imdbID" component={MovieDetail}/>
             <Route component={PageNotFound}/>
           </Switch>
+          </div>
           <Footer/>
         </Router>
     </div>
