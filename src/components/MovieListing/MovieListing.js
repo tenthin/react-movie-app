4c2892f95fdba565480020dab0234a767dd618a9
Tenzin <tenthinten@gmail.com>
2022-02-05 20:03:53 +0530

    mistake in bracket

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -9,9 +9,9 @@ const MovieListing = () => {
 
     renderMovies = 
         movies.Response === "True" ? (
-            movies.Search.map((movie,index) => {
-                <MovieCard key={index} data = {movie}/>
-            })
+            movies.Search.map((movie,index) => (
+                <MovieCard key={index} data={movie}/>
+            ))
         )   :   (
             <div className="movies-error">
                 <h3>{movies.Error}</h3>
